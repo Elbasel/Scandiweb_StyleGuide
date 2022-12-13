e41945abc0e7c271620869a7a88baf6d1014f6df
Abdelrahman Elbasel <abdelrahman.elbasel@scandiweb.com>
2022-12-13 15:49:17 +0200

    init commit

---
+++ YourCompany/YourModule/Block/Helloworld.php
@@ -0,0 +1,10 @@
+<?php   
+namespace YourCompany\YourModule\Block;
+
+class Helloworld extends \Magento\Framework\View\Element\Template
+{   
+    public function getHelloWorldTxt()   
+    {   
+        return 'Hello world!';   
+    }   
+}   
